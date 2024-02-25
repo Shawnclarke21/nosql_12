b23c0d3d96842427379f521b993aa3cf4d6b7df3
unknown <srclarke777@gmail.com>
2024-02-25 15:14:10 -0700

    updated my server.js file and started my departments.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const db =require('./db/connection');
+const express=require('express');
+const startInq=('./lib/departments')
+const PORT=process.env.PORT ||3001;
+const app=express();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use((req,res)=>{
+    res.status(404).end();
+})
+
+db.connect(err=>{
+    if(err)throw err;
+    console.log('connected to database');
+    app.listen(PORT,()=>{
+        console.log(`server running on ${PORT}`);
+        startInq();
+    });
+});
