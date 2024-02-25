37a1660dea389a32ac7e787fcf656a6c2ff9441c
unknown <srclarke777@gmail.com>
2024-02-25 15:45:18 -0700

    installed express and fixed syntax errors in the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,25 @@
 USE company;
+
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
-
-CREATE TABLE roles(
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30)NOT NULL,
-salary DECIMAL(10,2)NOT NULL,
-department_id INTEGER,
-
-
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER
 );
-CREATE TABLE departments(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHART(30) NOT NULL
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
-CREATE TABLE employees(
-id INTEGER AUTO_INCREMENT PRIMARY KEY;
-first_name VARCHAR(30) NOT NULL;
-last_name VARCHAR(30) NOT NULL;
-role_id INTEGER,
-manager_id INTEGER,
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
 );
